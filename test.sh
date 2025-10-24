e5cc92a09a1a0ec934e2c25b252fac757df8b2b5
Stephen Garriga <steve_garriga@yahoo.com>
2025-10-24 15:22:08 -0400

    Avoid unneccesary TESTDATA updates to github

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 for zipfile in TESTDATA/*.gz; do
     testfile=$(basename ${zipfile} .wav.gz)
-    echo "Testing ${zipfile} -> ${testfile}.wav -> ${testfile}.bmp"
-    gunzip ${zipfile}
-    ./slowrx-cli  -o TESTDATA/${testfile}.bmp TESTDATA/${testfile}.wav
-    gzip TESTDATA/${testfile}.wav
+    echo "Testing ${zipfile} -> ${testfile}.bmp"
+    # create a copy to not have to rezip and cause a git diff
+    cp ${zipfile} TESTDATA/_${testfile}.wav.gz
+    gunzip TESTDATA/_${testfile}.wav.gz
+    ./slowrx-cli  -o TESTDATA/${testfile}.bmp TESTDATA/_${testfile}.wav
+    rm TESTDATA/_${testfile}.wav
 done
